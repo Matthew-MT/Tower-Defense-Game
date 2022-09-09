948207cfa16d835448ab3faf2d1f1233b2f7d7d5
Lord-Chaos-YT <thumper540@gmail.com>
2022-09-09 11:13:18 -0600

    Testing SDL.

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <SDL2/SDL.h>
+
+int main(int argc, char *argv[]) {
+    SDL_Window *win = NULL;
+    SDL_Renderer *renderer = NULL;
+    SDL_Texture *bitmapTex = NULL;
+    SDL_Surface *bitmapSurface = NULL;
+    int posX = 100, posY = 100, width = 320, height = 240;
+    SDL_bool loopShouldStop = SDL_FALSE;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    win = SDL_CreateWindow("Hello World", posX, posY, width, height, 0);
+
+    renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+
+    bitmapSurface = SDL_LoadBMP("img/hello.bmp");
+    bitmapTex = SDL_CreateTextureFromSurface(renderer, bitmapSurface);
+    SDL_FreeSurface(bitmapSurface);
+
+    SDL_QueryTexture(bitmapTex, NULL, NULL, &width, &height);
+    SDL_SetWindowSize(win, width / 2, height / 2);
+
+    while (!loopShouldStop) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT: {
+                    loopShouldStop = SDL_TRUE;
+                    break;
+                }
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, bitmapTex, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyTexture(bitmapTex);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(win);
+
+    SDL_Quit();
+    return 0;
+}
