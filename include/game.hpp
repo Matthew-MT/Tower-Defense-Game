b3f1ded66937bac03da23900f6663a6f4ec49a3a
Lord-Chaos-YT <thumper540@gmail.com>
2022-09-09 11:47:15 -0600

    Game class now creates basic window

--- include/game.hpp
+++ include/game.hpp
@@ -6,9 +6,15 @@ namespace game {
     class Game {
     private:
         SDL_bool exit = SDL_FALSE;
-        SDL_Window* win = nullptr;
+        SDL_Window* window = nullptr;
         SDL_Renderer* renderer = nullptr;
     public:
-        Game(const std::string& title, int x, int y, int w, int h);
+        Game(const std::string& title, int x, int y, int w, int h) {
+            this->window = SDL_CreateWindow(title.c_str(), x, y, w, h, 0);
+            this->renderer = SDL_CreateRenderer(this->window, -1, SDL_RENDERER_ACCELERATED);
+        }
+        ~Game() {
+            SDL_DestroyWindow(this->window);
+        }
     };
 };
