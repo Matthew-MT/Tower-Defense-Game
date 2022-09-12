76f0063db03e0e53acba027f3abb03b046d2fc5d
Lord-Chaos-YT <thumper540@gmail.com>
2022-09-12 11:30:29 -0600

    Adding some map logic

--- include/map.hpp
+++ include/map.hpp
@@ -1,6 +1,26 @@
 #pragma once
 #include <SDL2/SDL.h>
+#include <fstream>
+#include <vector>
+#include <string>
 
 namespace game {
-    class Map {};
+    class Map {
+    private:
+        std::vector<std::vector<int>> map;
+    public:
+        Map(const std::string& mapFileName) {
+            std::fstream mapFile(mapFileName, std::ios_base::in);
+            std::string buffer;
+            while (mapFile.eof()) {
+                std::getline(mapFile, buffer);
+                map.push_back({});
+                for (char c : buffer) {
+                    if (c == ',') map.back().push_back(0);
+                    else (map.back().back() *= 10) += (int)(c - '0');
+                }
+            }
+        }
+        ~Map() {}
+    };
 };
