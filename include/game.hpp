b1ddd46c63ae4c71b63e688820ba3623728c9d6d
Lord-Chaos-YT <thumper540@gmail.com>
2022-09-14 11:51:11 -0600

    Adding more framework to build the main game screen.

--- include/game.hpp
+++ include/game.hpp
@@ -1,20 +1,75 @@
 #pragma once
 #include <SDL2/SDL.h>
 #include <string>
+#include <vector>
 
 namespace game {
-    class RenderableTexture {
+    template<typename Type> class Point {
+    public:
+        Type x, y;
+        Point(Type ix, Type iy) : x{ix}, y{iy} {}
+    };
+
+    using IPoint = Point<int>;
+
+    class Sprite {
     private:
+        SDL_Renderer* renderer;
         SDL_Texture* texture;
         SDL_Rect* sourceRect = nullptr;
         SDL_Rect* destRect = nullptr;
+        double angle = 0.f;
     public:
-        RenderableTexture() {}
-        ~RenderableTexture() {}
-        void render(SDL_Renderer* renderer) {
-            SDL_RenderCopy(renderer, this->texture, this->sourceRect, this->destRect);
+        Sprite(
+            SDL_Renderer* initRenderer,
+            SDL_Texture* initTexture,
+            SDL_Rect* initSourceRect,
+            SDL_Rect* initDestRect
+        ) :
+            renderer{initRenderer},
+            texture{initTexture},
+            sourceRect{initSourceRect},
+            destRect{initDestRect} {}
+        ~Sprite() {}
+        void render() {
+            SDL_RenderCopyEx(
+                this->renderer,
+                this->texture,
+                this->sourceRect,
+                this->destRect,
+                this->angle,
+                nullptr,
+                SDL_FLIP_NONE
+            );
         }
-        void setSourceRect(int x, int y, int w, int h) {}
+        void setTexture(SDL_Texture* texture) {
+            this->texture = texture;
+        }
+        void setSourceRect(SDL_Rect* newSourceRect) {
+            this->sourceRect = newSourceRect;
+        }
+        void setDestRect(SDL_Rect* newDestRect) {
+            this->destRect = newDestRect;
+        }
+        void setPosition(int x, int y) {
+            this->destRect->x = x;
+            this->destRect->y = y;
+        }
+        void setAngle(double angle) {
+            this->angle = angle;
+        }
+        const SDL_Rect* getSourceRect() {
+            return this->sourceRect;
+        }
+        const SDL_Rect* getDestRect() {
+            return this->destRect;
+        }
+        IPoint getPosition() {
+            return {this->destRect->x, this->destRect->y};
+        }
+        double getAngle(double angle) {
+            return this->angle;
+        }
     };
 
     class Game {
@@ -22,6 +77,7 @@ namespace game {
         SDL_bool exit = SDL_FALSE;
         SDL_Window* window = nullptr;
         SDL_Renderer* renderer = nullptr;
+        std::vector<Sprite> renderList;
     public:
         Game(const std::string& title, int x, int y, int w, int h) {
             this->window = SDL_CreateWindow(title.c_str(), x, y, w, h, 0);
@@ -32,6 +88,11 @@ namespace game {
         }
         void render() {
             SDL_RenderClear(this->renderer);
+            for (Sprite sprite : this->renderList) sprite.render();
+            SDL_RenderPresent(this->renderer);
+        }
+        void spawnSprite(Sprite& sprite) {
+            this->renderList.push_back(sprite);
         }
     };
 };
