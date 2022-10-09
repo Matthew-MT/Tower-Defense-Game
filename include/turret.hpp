6ee5917d26f697d90cf98d3a1a53f6b13e39a237
ejmccallum <ejmccallum@mavs.coloradomesa.edu>
2022-10-09 17:51:44 -0600

    added turret_h.hpp file

--- include/turret.hpp
+++ include/turret.hpp
@@ -4,132 +4,109 @@
 #include "sprite.hpp"
 #include "map.hpp"
 #include "forward.hpp"
+#include "turret_h.hpp"
 
 namespace game{
-    class Turret : public Sprite
+    Turret::Turret(
+        SDL_Renderer* initRenderer,
+        SDL_Texture* initTexture,
+        SDL_Rect* initDestRect,
+        SDL_Rect* initSourceRect = nullptr,
+        int initDamage=0,
+        double initReload=0.0
+    ) : Sprite {
+        initRenderer, 
+        initTexture, 
+        initDestRect,
+        initSourceRect
+    },
+    damage{initDamage},
+    reloadTime{initReload}{}
+
+    void Turret::currentEnemy(){
+
+    }
+    Turret::~Turret(){}
+
+
+
+    TurretHandler::TurretHandler(
+        SDL_Renderer* initRenderer,
+        SDL_Rect* initDestRect
+        ) : Renderable{initRenderer, initDestRect}
+        {
+            readTurretData("turret.txt");
+            storeTurretData(texture, damage, reloadTime);
+        }
+
+    void TurretHandler::render()
     {
-        protected:
-        GameState* gamestate;
-        TurretHandler* handler;
-        int damage;
-        float reloadTime;
-        public:
-        Turret(
-            SDL_Renderer* initRenderer,
-            SDL_Texture* initTexture,
-            SDL_Rect* initDestRect,
-            SDL_Rect* initSourceRect = nullptr,
-            int initDamage=0,
-            double initReload=0.0
-        ) : Sprite {
-            initRenderer, 
-            initTexture, 
-            initDestRect,
-            initSourceRect
-        },
-        damage{initDamage},
-        reloadTime{initReload} {}
-
-        void currentEnemy(){
-
+        for(Turret* turret: this->turrets)
+        {
+            turret->render();
         }
-        ~Turret() {}
-    };
+    }    
 
-    class TurretHandler : public Renderable
+    void  TurretHandler::readTurretData(const std::string& turretFileName)
     {
-        protected:
-        Map* map;
-        std::unordered_set<Turret*> turrets;
-        std::vector<TurretData*>  turretTypes;
-        int damage;
-        float reloadTime;
-        SDL_Texture* texture;
-        public:
-        TurretHandler(
-            SDL_Renderer* initRenderer,
-            SDL_Rect* initDestRect
-            ) : Renderable{initRenderer, initDestRect}
+        std::fstream 
+            turretFile("assets/turrets/" + turretFileName, std::ios_base::in);
+        std::string buffer;
+
+        std::getline(turretFile, buffer);
+        damage = std::stoi(buffer);
+        std::getline(turretFile, buffer);
+        reloadTime = std::stof(buffer);
+        std::getline(turretFile, buffer);
+        SDL_Surface* surface = SDL_LoadBMP(((std::string)"assets/images/" + buffer).c_str());
+        texture = SDL_CreateTextureFromSurface(this->renderer, surface);
+        SDL_FreeSurface(surface);
+    }
+
+    void  TurretHandler::storeTurretData(SDL_Texture* t, int d, float r)
+    {
+        turretTypes.push_back(new TurretData(d, r, t));
+    }
+
+    void  TurretHandler::createTurret(TurretType type)
+    {
+        TurretData* data = this->turretTypes[type];
+        this->turrets.insert(new Turret(
+            this->renderer,
+            data->texture,
+            this->destRect,
+            nullptr,
+            data->damage,
+            data->reload
+        ));
+    }
+
+    void  TurretHandler::handleEvent(SDL_Event* event)
+    {
+        int x, y;
+        SDL_Log("Handling event");
+        if(event->type == SDL_MOUSEBUTTONUP)
+        {
+            SDL_GetGlobalMouseState(&x, &y);
+            IPoint index = this->map->getTileIndex({x,y});
+            std::cout << index.x << "   " << index.y << std::endl;
+            bool placed = this->map->placeTurret(index);
+            if(placed)
             {
-                readTurretData("turret.txt");
-                storeTurretData(texture, damage, reloadTime);
-            }
-
-        void render()
-        {
-            for(Turret* turret: this->turrets)
-            {
-                turret->render();
-            }
-        }    
-
-        void readTurretData(const std::string& turretFileName)
-        {
-            std::fstream 
-                turretFile("assets/turrets/" + turretFileName, std::ios_base::in);
-            std::string buffer;
-
-            std::getline(turretFile, buffer);
-            damage = std::stoi(buffer);
-            std::getline(turretFile, buffer);
-            reloadTime = std::stof(buffer);
-            std::getline(turretFile, buffer);
-            SDL_Surface* surface = SDL_LoadBMP(((std::string)"assets/images/" + buffer).c_str());
-            texture = SDL_CreateTextureFromSurface(this->renderer, surface);
-            SDL_FreeSurface(surface);
-        }
-
-        void storeTurretData(SDL_Texture* t, int d, float r)
-        {
-            turretTypes.push_back(new TurretData(d, r, t));
-        }
-
-        void createTurret(TurretType type)
-        {
-            TurretData* data = this->turretTypes[type];
-            this->turrets.insert(new Turret(
-                this->renderer,
-                data->texture,
-                this->destRect,
-                nullptr,
-                data->damage,
-                data->reload
-            ));
-        }
-
-        void handleEvent(SDL_Event* event)
-        {
-            int x, y;
-            SDL_Log("Handling event");
-            if(event->type == SDL_MOUSEBUTTONUP)
-            {
-                SDL_GetGlobalMouseState(&x, &y);
-                IPoint index = this->map->getTileIndex({x,y});
-                std::cout << index.x << "   " << index.y << std::endl;
-                bool placed = this->map->placeTurret(index);
-                if(placed)
-                {
-                    SDL_Log("Turret was placed");
-                    createTurret(Gatling);
-                }
+                SDL_Log("Turret was placed");
+                createTurret(Gatling);
             }
         }
-    };
+    }
 
-    class TurretData
+    TurretData::TurretData(
+        int initDamage, 
+        float initReload, 
+        SDL_Texture* initTexture
+    )
     {
-        public:
-        int damage;
-        float reload;
-        SDL_Texture* texture;
-        public:
-        TurretData(
-            int initDamage, float initReload, SDL_Texture* initTexture
-        )
-        {
-            damage = initDamage;
-            reload = initReload;
-            texture = initTexture;
-        }
-    };
-}
+        damage = initDamage;
+        reload = initReload;
+        texture = initTexture;
+    }
+};
---
+++ include/turret_h.hpp
@@ -0,0 +1,66 @@
+#pragma once
+#include <SDL2/SDL.h>
+#include "game_state.hpp"
+#include "sprite.hpp"
+#include "map.hpp"
+#include "forward.hpp"
+
+namespace game{
+    class Turret : public Sprite
+    {
+        protected:
+        GameState* gamestate;
+        int damage;
+        float reloadTime;
+        public:
+        Turret(
+            SDL_Renderer* initRenderer,
+            SDL_Texture* initTexture,
+            SDL_Rect* initDestRect,
+            SDL_Rect* initSourceRect = nullptr,
+            int initDamage=0,
+            double initReload=0.0
+        );
+
+        void currentEnemy();
+        ~Turret();
+    };
+
+    class TurretHandler : public Renderable
+    {
+        protected:
+        Map* map;
+        std::unordered_set<Turret*> turrets;
+        std::vector<TurretData*>  turretTypes;
+        int damage;
+        float reloadTime;
+        SDL_Texture* texture;
+        public:
+        TurretHandler(
+            SDL_Renderer* initRenderer,
+            SDL_Rect* initDestRect
+            ); 
+
+        void render();
+
+        void readTurretData(const std::string& turretFileName);
+
+        void storeTurretData(SDL_Texture* t, int d, float r);
+
+        void createTurret(TurretType type);
+
+        void handleEvent(SDL_Event* event);
+    };
+
+    class TurretData
+    {
+        public:
+        int damage;
+        float reload;
+        SDL_Texture* texture;
+        public:
+        TurretData(
+            int initDamage, float initReload, SDL_Texture* initTexture
+        );
+    };
+};
