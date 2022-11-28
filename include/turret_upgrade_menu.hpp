89a8903e6cfc6ffad044d110865efe0125e65483
Lord-Chaos-YT <thumper540@gmail.com>
2022-11-28 11:46:29 -0700

    Still working on the turret upgrade menu.

--- include/turret_upgrade_menu.hpp
+++ include/turret_upgrade_menu.hpp
@@ -6,10 +6,30 @@
 namespace game {
     TurretUpgradeMenu::TurretUpgradeMenu(
         SDL_Renderer* initRenderer
-    ) : StaticSprite{
-        initRenderer,
-        textureFromImageFile(initRenderer, "assets/images/turret_upgrade_menu.png"),
-        nullptr,
-        nullptr
-    } {}
+    ) :
+        StaticSprite{
+            initRenderer,
+            textureFromImageFile(initRenderer, "assets/images/turret_upgrade_menu.png"),
+            nullptr,
+            nullptr
+        },
+        sell{textureFromImageFile(initRenderer, "assets/images/turret_sell.png")},
+        upgrade{textureFromImageFile(initRenderer, "assets/images/turret_upgrade.png")} {}
+
+    void TurretUpgradeMenu::render() {
+        if (!shown) return;
+        this->StaticSprite::render();
+        if (this->sell != nullptr) SDL_RenderCopy(
+            this->renderer,
+            this->sell
+        );
+    }
+
+    void TurretUpgradeMenu::handleEvent(SDL_Event* event) {}
+
+    void TurretUpgradeMenu::loadMenuFor(TurretData* type, const IPoint& center) {}
+
+    void TurretUpgradeMenu::closeMenu() {
+        this->shown = false;
+    }
 };
--- include/turret_upgrade_menu_h.hpp
+++ include/turret_upgrade_menu_h.hpp
@@ -1,13 +1,32 @@
 #pragma once
 #include "forward.hpp"
+#include "turret_h.hpp"
 #include "sprite.hpp"
+#include "text.hpp"
 
 namespace game {
     class TurretUpgradeMenu : public StaticSprite {
     protected:
+        SDL_Texture
+            * sell = nullptr,
+            * upgrade = nullptr;
+        Text
+            * sellText = nullptr,
+            * upgradeText = nullptr;
+        SDL_Rect
+            * sellRect = nullptr,
+            * upgradeRect = nullptr;
+        bool shown = false;
+
+        virtual void updateRects();
     public:
         TurretUpgradeMenu(
             SDL_Renderer* initRenderer
         );
+
+        virtual void render();
+        virtual void handleEvent(SDL_Event* event);
+        virtual void loadMenuFor(TurretData* type, const IPoint& center);
+        virtual void closeMenu();
     };
 };
