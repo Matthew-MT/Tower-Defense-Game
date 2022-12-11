fd37490204318e104a0ff47c3a96932eda06c7d6
Lord-Chaos-YT <thumper540@gmail.com>
2022-12-11 14:32:53 -0700

    Likely won't have time to finish this before the presentation. Instead, this current change works so that we can demo it.

--- include/pathfinder.hpp
+++ include/pathfinder.hpp
@@ -6,6 +6,54 @@
 #include <vector>
 
 namespace game {
+    Path::Neighborhood::Neighborhood(
+        const IPoint& initActual,
+        const IPoint& initOrigin,
+        const IPoint& initCurrent,
+        const IPoint& initTarget
+    ) :
+        actual{initActual},
+        origin{initOrigin},
+        current{initCurrent},
+        target{initTarget} {}
+
+    Path::Neighborhood Path::getNeighborhood(const DPoint& currentPosition) {
+        IPoint actual = this->map->getTileIndex(currentPosition);
+
+        std::vector<IPoint>::iterator current = std::find(this->path.begin(), this->path.end(), actual);
+        if (current == this->path.end()) {
+            std::vector<IPoint>::reverse_iterator r = std::find_if(
+                this->path.rbegin(),
+                this->path.rend(),
+                [&](const IPoint& point) -> bool {
+                    return
+                        point.x <= actual.x + 1
+                        && point.x >= actual.x - 1
+                        && point.y <= actual.y + 1
+                        && point.y >= actual.y - 1;
+                }
+            );
+
+            if (r == this->path.rend()) {
+                SDL_Log("Pathfinders must receive a new path when paths are recomputed.");
+                throw 1;
+            }
+
+            current = (++r).base();
+        }
+
+        std::vector<IPoint>::iterator origin = current, target = current;
+        if (origin != this->path.begin()) origin--;
+        if (target + 1 != this->path.end()) target++;
+
+        return {
+            actual,
+            *origin,
+            *current,
+            *target
+        };
+    }
+
     Path::Path(Map* initMap, const std::vector<IPoint>& initPath) : map{initMap}, path{initPath} {}
     Path::~Path() {}
 
@@ -13,7 +61,7 @@ namespace game {
         return std::find(this->path.begin(), this->path.end(), index) != this->path.end();
     }
 
-    DPoint Path::next(double scalar, const DPoint& currentPosition, int movementSpeed) {        
+    DPoint Path::next(double scalar, const DPoint& currentPosition, int movementSpeed) {
         if (movementSpeed < 0) throw "Positive movement speed only.";
 
         IPoint index = this->map->getTileIndex(currentPosition);
@@ -58,19 +106,34 @@ namespace game {
             } else return {std::numeric_limits<double>::max(), std::numeric_limits<double>::max()};
         }
 
-        SDL_Log((
-            "\nCurrent: (" + std::to_string(currentPosition.x) + ", " + std::to_string(currentPosition.y) + ")\n"
-            + "Origin:  (" + std::to_string(originPosition.x) + ", " + std::to_string(originPosition.y) + ")\n"
-            + "Target:  (" + std::to_string(targetPosition.x) + ", " + std::to_string(targetPosition.y) + ")"
-        ).c_str());
+        // SDL_Log((
+        //     "\nCurrent: (" + std::to_string(currentPosition.x) + ", " + std::to_string(currentPosition.y) + ")\n"
+        //     + "Origin:  (" + std::to_string(originPosition.x) + ", " + std::to_string(originPosition.y) + ")\n"
+        //     + "Target:  (" + std::to_string(targetPosition.x) + ", " + std::to_string(targetPosition.y) + ")"
+        // ).c_str());
 
-        DPoint slope = targetPosition;
-        
-        if (originPosition == targetPosition) slope -= currentPosition;
-        else slope -= originPosition;
+        DPoint slope = targetPosition - currentPosition;
+
+        // if (originPosition == targetPosition) slope -= currentPosition;
+        // else slope -= originPosition;
 
         // double normalizeScalar = std::sqrt(std::pow((double)slope.x, 2) + std::pow((double)slope.y, 2)) / range;
 
         return currentPosition + slope.normal(range);
     }
+
+    // DPoint Path::next(double scalar, const DPoint& currentPosition, int movementSpeed) {
+    //     const Path::Neighborhood nh = this->getNeighborhood(currentPosition);
+    //     IPoint size = this->map->getTileSize();
+
+    //     if (nh.origin == nh.current + this->n) {
+    //         if (nh.target == nh.current + this->e) {
+    //             IPoint corner = this->map->getTileCenter(nh.current);
+    //             corner.x += size.x >> 1;
+    //             corner.y -= size.y >> 1;
+
+    //             if (distance(currentPosition, (DPoint)corner) >= size.x)
+    //         }
+    //     }
+    // }
 };
--- include/pathfinder_h.hpp
+++ include/pathfinder_h.hpp
@@ -9,6 +9,34 @@ namespace game {
     protected:
         Map* map;
         std::vector<IPoint> path;
+
+        const IPoint
+            n = {0, -1},
+            ne = {1, -1},
+            e = {1, 0},
+            se = {1, 1},
+            s = {0, 1},
+            sw = {-1, 1},
+            w = {-1, 0},
+            nw = {-1, -1};
+
+        class Neighborhood {
+        public:
+            IPoint
+                actual,
+                origin,
+                current,
+                target;
+
+            Neighborhood(
+                const IPoint& initActual,
+                const IPoint& initOrigin,
+                const IPoint& initCurrent,
+                const IPoint& initTarget
+            );
+        };
+
+        virtual Neighborhood getNeighborhood(const DPoint& currentPosition);
     public:
         Path(Map* initMap, const std::vector<IPoint>& initPath);
         ~Path();
